0095b8431bf3a2eaf8f14b8a837e660f65652bab
Josep Subils <josep.subils@dxc.com>
2021-01-17 12:06:46 +0100

    framework documentacio

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # teletram-bot-picateclas
 
 Bot de Telegram per el grup picateclas
+
+### Framework escollit:
+
+https://telegraf.js.org/
+https://github.com/telegraf/telegraf
--- bot.js
+++ bot.js
@@ -2,11 +2,8 @@ require("dotenv").config()
 
 const { Telegraf } = require("telegraf")
 
-console.log(process.env.BOT_TOKEN)
-
-/*
-// EXEMPLE
 const bot = new Telegraf(process.env.BOT_TOKEN)
+
 bot.start(ctx => ctx.reply("Welcome"))
 bot.help(ctx => ctx.reply("Send me a sticker"))
 bot.on("sticker", ctx => ctx.reply("👍"))
@@ -15,4 +12,3 @@ bot.launch()
 
 process.once("SIGINT", () => bot.stop("SIGINT"))
 process.once("SIGTERM", () => bot.stop("SIGTERM"))
-*/
