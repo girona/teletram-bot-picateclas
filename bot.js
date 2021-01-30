6064c4eecc16793ddf33e6d906e2304e495c5513
Eudald Dachs Gonzalez <eudago@MacBook-Pro-de-Eudald.local>
2021-01-30 17:34:50 +0100

    add default topics and check in title

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 require("dotenv").config()
 
 const { Telegraf } = require("telegraf")
-const topics = require("./topics");
+const {Topics, TopicsList} = require("./topics");
 const wait = hores => new Promise(resolve => setTimeout(resolve, hores * 60 * 60 * 1000))
-const news = require('./news')
-const db = require('./fake_db')
+const News = require('./news')
+const DB = require('./fake_db')
 const bot = new Telegraf(process.env.BOT_TOKEN)
-const chatId = process.env.CHAT_ID;
+const chatId = process.env.CHAT_ID
 
 bot.start(ctx => ctx.reply("Bot iniciat!"))
 
@@ -28,15 +28,17 @@ bot.hears("saluda", ctx => {
   ctx.telegram.sendMessage(chatId, "Salutacions desde el bot")
 })
 
-topics(bot);
+Topics(bot);
 
 const main = async () => {
   for (;;) {
-    let news_items = await news.genbetaNews() 
+    let news_items = await News.genbetaNews() 
     for(let n of news_items.slice(1, 2)) { //només 1 noticia
-        db.add(n.link);
+      if(TopicsList.some(topic => n.title.includes(topic))) {
+        DB.add(n.link);
         bot.telegram.sendMessage(chatId, n.link)
             .catch((err) => console.log(err))
+      } 
     }
     await wait(2)
   }
---
+++ topics/default.js
@@ -0,0 +1,8 @@
+const DefaultTopics = [
+    'kubernetes',
+    'go',
+    'javascript',
+    'girona'
+]
+
+module.exports = DefaultTopics;
