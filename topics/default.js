f37daf6d2b5a5c13045bad895d609e9a054cfae9
Josep Subils <josep.subils@dxc.com>
2021-02-02 19:16:11 +0100

    Afegit golang al llistat de topics

--- topics/default.js
+++ topics/default.js
@@ -1,9 +1,3 @@
-const DefaultTopics = [
-    'kubernetes',
-    ' go ',
-    'javascript',
-    'girona',
-    'python'
-]
+const DefaultTopics = ["kubernetes", " go ", "golang", "javascript", "girona", "python"]
 
-module.exports = DefaultTopics;
+module.exports = DefaultTopics
